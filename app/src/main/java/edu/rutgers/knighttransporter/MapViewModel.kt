e2fdaec30a92bea09bdcda2f111872459bec1073
Miles Krell <repos@mileskrell.com>
2020-04-14 17:13:05 -0400

    Get bus data from Socket.IO server

--- app/src/main/java/edu/rutgers/knighttransporter/MapViewModel.kt
+++ app/src/main/java/edu/rutgers/knighttransporter/MapViewModel.kt
@@ -1,11 +1,20 @@
 package edu.rutgers.knighttransporter
 
 import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import edu.rutgers.knighttransporter.for_non_mapbox_queries.Repository
+import edu.rutgers.knighttransporter.for_transloc.Route
 
 class MapViewModel : ViewModel() {
-    val repository = Repository()
+    private val _routes = MutableLiveData<List<Route>>(emptyList())
+    val routes: LiveData<List<Route>>
+        get() = _routes
+
+    private val repository = Repository { newRoutes ->
+        _routes.postValue(newRoutes)
+    }
+
     val mapInstanceState = Bundle()
 
     suspend fun getWalkways() = repository.getWalkways()
---
+++ app/src/main/java/edu/rutgers/knighttransporter/Repository.kt
@@ -0,0 +1,62 @@
+package edu.rutgers.knighttransporter
+
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.mapbox.geojson.FeatureCollection
+import edu.rutgers.knighttransporter.for_non_mapbox_queries.GeoJSONService
+import edu.rutgers.knighttransporter.for_transloc.Route
+import io.socket.client.IO
+import io.socket.client.Socket
+import org.json.JSONArray
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+class Repository(val onRoutesUpdated: (routes: List<Route>) -> Unit) {
+    companion object {
+        const val TAG = "Repository"
+    }
+
+    private var walkways: FeatureCollection? = null
+    private var parkingLots: FeatureCollection? = null
+    private var buildings: FeatureCollection? = null
+
+    private val translocSocket = IO.socket(translocUrl).connect().apply {
+        on(Socket.EVENT_CONNECT) {
+            Log.d(TAG, "Connected")
+        }.on(Socket.EVENT_CONNECTING) {
+            Log.d(TAG, "Connecting")
+        }.on(Socket.EVENT_CONNECT_TIMEOUT) {
+            Log.d(TAG, "Connect timeout")
+        }.on(Socket.EVENT_CONNECT_ERROR) {
+            Log.d(TAG, "Connect error")
+        }.on("data") {
+            Log.d(TAG, "Received data")
+            val routes = Gson().fromJson<List<Route>>(
+                (it[0] as JSONArray).toString(),
+                object : TypeToken<List<Route>>() {}.type
+            )
+            onRoutesUpdated(routes)
+        }.on(Socket.EVENT_DISCONNECT) {
+            Log.d(TAG, "Disconnected")
+        }
+    }
+
+    val geoJSONService = Retrofit.Builder()
+        .baseUrl(arcGISbaseUrl)
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .build()
+        .create(GeoJSONService::class.java)
+
+    suspend fun getWalkways() = walkways ?: geoJSONService.getWalkways().let {
+        return FeatureCollection.fromJson(it)
+    }
+
+    suspend fun getParkingLots() = parkingLots ?: geoJSONService.getParkingLots().let {
+        return FeatureCollection.fromJson(it)
+    }
+
+    suspend fun getBuildings() = buildings ?: geoJSONService.getBuildings().let {
+        return FeatureCollection.fromJson(it)
+    }
+}
