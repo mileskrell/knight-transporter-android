823974978302d0ceef324e82ea12b56f2c61d67a
Miles Krell <noreply@mileskrell.com>
2020-04-24 13:58:12 -0400

    Create fragments for bottom sheet

---
+++ app/src/main/java/edu/rutgers/knighttransporter/bottom_sheets/BottomSheetConstants.kt
@@ -0,0 +1,3 @@
+package edu.rutgers.knighttransporter.bottom_sheets
+
+const val ARG_FEATURE = "feature"
---
+++ app/src/main/java/edu/rutgers/knighttransporter/bottom_sheets/StopFragment.kt
@@ -0,0 +1,37 @@
+package edu.rutgers.knighttransporter.bottom_sheets
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.mapbox.geojson.Feature
+import edu.rutgers.knighttransporter.R
+import edu.rutgers.knighttransporter.for_non_mapbox_queries.PlaceType
+import edu.rutgers.knighttransporter.for_non_mapbox_queries.getNameForPlaceType
+import kotlinx.android.synthetic.main.fragment_place_sheet_stop.*
+
+class StopFragment : Fragment(R.layout.fragment_place_sheet_stop) {
+    companion object {
+        @JvmStatic
+        fun newInstance(feature: String) =
+            StopFragment()
+                .apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_FEATURE, feature)
+                    }
+                }
+    }
+
+    private lateinit var feature: Feature
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            feature = Feature.fromJson(it.getString(ARG_FEATURE)!!)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        place_sheet_stop_name.text = feature.getNameForPlaceType(PlaceType.STOP)
+    }
+}
